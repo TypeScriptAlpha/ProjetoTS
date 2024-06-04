05c7b536e7cc3f92c98ce75bcf0ad019d9c8566a
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2024-06-04 15:33:54 -0300

    deleting curls comands

--- src/useCases/GetTeamMembers/GetTeamMembersController.ts
+++ src/useCases/GetTeamMembers/GetTeamMembersController.ts
@@ -15,5 +15,3 @@ export class GetTeamMembersController {
         }
     }
 }
-// curl -X GET http://localhost:3000/teams/8f36eca7-8195-4f1c-83f2-d7b3722b64c6/members \
-// -b "session_id=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImEwOTZhY2NmLTQ4YjUtNDdmMi1hYWY2LTg0MGE2MDkxMDY5ZCIsImlzX2FkbWluIjpmYWxzZSwibGVhZGVyIjoiOGYzNmVjYTctODE5NS00ZjFjLTgzZjItZDdiMzcyMmI2NGM2IiwiaWF0IjoxNzE3MzU3NjE3LCJleHAiOjE3MTgyMjE2MTd9.MaMsfiG0w_8-9llfCELFlUdEq78fIJ5FRDba2RkHCOg"

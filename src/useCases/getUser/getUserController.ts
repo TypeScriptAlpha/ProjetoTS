a23bbe543592b76afd2e4ec5ddff4a94b90b1b67
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2024-06-04 15:31:08 -0300

    specifying error messages and response status from GetUserController

--- src/useCases/getUser/getUserController.ts
+++ src/useCases/getUser/getUserController.ts
@@ -9,18 +9,26 @@ export class GetUserController {
         const { user_id } = req.params;
         const requester_id = req.user?.id || '';
         const is_admin = req.user?.is_admin || false;
-        const requester_leader_id = req.user?.leader || '' ;  // Se existir, indica que é líder de uma equipe
+        const requester_leader_id = req.user?.leader || '' ;
 
         try {
             const user = await this.getUserUseCase.execute(user_id, requester_id, is_admin, requester_leader_id);
+
+            if (!user) {
+                return res.status(404).json({ error: "User not found" });
+            }
+
             return res.status(200).json(user);
-        } catch (error) {
+        } catch (error: any) {
             if (error instanceof HttpError) {
                 return res.status(error.status).json({ error: error.message });
+            } else if (error.message === 'Permission denied') {
+                return res.status(403).json({ error: "Permission denied" });
+            } else if (error.message === 'Invalid user ID') {
+                return res.status(400).json({ error: "Invalid user ID" });
+            } else {
+                return res.status(500).json({ error: 'Internal server error' });
             }
-            return res.status(500).json({ error: 'Internal server error' });
         }
     }
 }
-// curl -X GET http://localhost:3000/users/582a0898-3d5a-4aff-a414-419736485ef1 \
-// -b "session_id=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImEwOTZhY2NmLTQ4YjUtNDdmMi1hYWY2LTg0MGE2MDkxMDY5ZCIsImlzX2FkbWluIjpmYWxzZSwibGVhZGVyIjoiOGYzNmVjYTctODE5NS00ZjFjLTgzZjItZDdiMzcyMmI2NGM2IiwiaWF0IjoxNzE3MzU3NjE3LCJleHAiOjE3MTgyMjE2MTd9.MaMsfiG0w_8-9llfCELFlUdEq78fIJ5FRDba2RkHCOg"
